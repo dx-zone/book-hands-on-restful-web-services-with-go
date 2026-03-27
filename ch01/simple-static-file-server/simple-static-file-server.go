fc05efdb08228c5f96df3bd0d605e004f04c2c00
Daniel <14101607+dx-zone@users.noreply.github.com>
2026-03-27 16:22:49 -0400

    chore: add commented-out CORS middleware to static file server

--- ch01/simple-static-file-server/simple-static-file-server.go
+++ ch01/simple-static-file-server/simple-static-file-server.go
@@ -20,8 +20,14 @@ func main() {
 	// Serve files from the root directory, endpoint is "/*filepath"
 	router.ServeFiles("//*filepath", http.Dir("/"))
 
+	// Enable CORS for all routes (optional)
+	//handler := cors.Default().Handler(router)
+
 	// Start the server
 	port := ":8080"
 	log.Printf("Starting server on port %s", port)
 	log.Fatal(http.ListenAndServe(port, router))
+
+	// If you want to use CORS, replace the above line with the following:
+	//log.Fatal(http.ListenAndServe(port, handler))
 }
